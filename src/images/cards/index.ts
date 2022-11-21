8ad22063a0405921c81b3543be76de13575442a4
Dario <dario.sehovic@icloud.com>
2022-11-21 14:12:20 +0100

    📷 add card images

---
+++ src/images/cards/index.ts
@@ -0,0 +1,39 @@
+import C01 from './01.svg';
+import C02 from './02.svg';
+import C03 from './03.svg';
+import C04 from './04.svg';
+import C05 from './05.svg';
+import C06 from './06.svg';
+import C07 from './07.svg';
+import C08 from './08.svg';
+import C09 from './09.svg';
+import C10 from './10.svg';
+import C11 from './11.svg';
+import C12 from './12.svg';
+import C13 from './13.svg';
+import C14 from './14.svg';
+import C15 from './15.svg';
+import C16 from './16.svg';
+import C17 from './17.svg';
+import C18 from './18.svg';
+
+export default {
+  1: C01,
+  2: C02,
+  3: C03,
+  4: C04,
+  5: C05,
+  6: C06,
+  7: C07,
+  8: C08,
+  9: C09,
+  10: C10,
+  11: C11,
+  12: C12,
+  13: C13,
+  14: C14,
+  15: C15,
+  16: C16,
+  17: C17,
+  18: C18,
+};
